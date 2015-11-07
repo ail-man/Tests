708c89a3ef21b84da135f67757f9a414a4fb6f40
Artur Lomsadze <ailman1985@gmail.com>
2015-11-07 22:01:23 +0300

    JavaEE 7.0 support + maven compiler plugin added Some JUnit testcase explained

---
+++ src/main/java/com/ail/home/servlet/ApplicationMainServlet.java
@@ -0,0 +1,25 @@
+package com.ail.home.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ApplicationMainServlet", urlPatterns = "/pages/*")
+public class ApplicationMainServlet extends javax.servlet.http.HttpServlet {
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        String body = "<html><body>%s</body></html>";
+        pw.printf(body, "It Works!");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
---
+++ src/test/java/com/ail/home/fortest/DoSomethingTest.java
@@ -0,0 +1,52 @@
+package com.ail.home.fortest;
+
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+public class DoSomethingTest extends Assert {
+
+    @Rule
+    public final Timeout timeout = new Timeout(1000, TimeUnit.MILLISECONDS);
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    @Test
+    public void throwsExceptionWithSpecificType() {
+        thrown.expect(NullPointerException.class);
+        throw new NullPointerException();
+    }
+
+    public static final String TEST = "ABCD";
+    private DoSomething ds;
+
+    @Before
+    public void newObject() {
+        ds = new DoSomething();
+    }
+
+    @Test
+    public void shouldReturnString() throws Exception {
+        String result = ds.execute(() -> TEST);
+        assertEquals(TEST, result);
+    }
+
+    @Test
+    public void shouldFilterResult() throws Exception {
+        Optional<String> result = ds.list()
+                .stream()
+                .map((a) -> a.toUpperCase())
+                .reduce((a, b) -> a + b);
+        assertTrue(result.isPresent());
+        assertEquals(TEST, result.get());
+    }
+
+    @Ignore
+    @Test
+    public void anotherInfinity() {
+        while (true);
+    }
+}
