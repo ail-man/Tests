40651f0e703a7fcfa86034bbec279696d03bc859
Artur Lomsadze <ailman1985@gmail.com>
2017-02-19 15:25:06 +0300

    Added serialization tests 7

--- src/main/java/com/ail/home/serialization/Person.java
+++ src/main/java/com/ail/home/serialization/Person.java
@@ -3,6 +3,7 @@ package com.ail.home.serialization;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.ObjectStreamException;
 import java.io.Serializable;
 
 public class Person implements Serializable {
@@ -12,8 +13,8 @@ public class Person implements Serializable {
 	private String firstName;
 	private String lastName;
 	private int age;
-	private Person spouse;
 	private Gender gender;
+	private Person spouse;
 
 	public Person(String fn, String ln, int a, Gender gender) {
 		this.firstName = fn;
@@ -32,6 +33,10 @@ public class Person implements Serializable {
 		stream.defaultReadObject();
 		// "Decrypt"/de-obscure the sensitive data
 		age = age << 2;
+	}
+
+	private Object writeReplace() throws ObjectStreamException {
+		return new PersonProxy(this);
 	}
 
 	public String getFirstName() {
@@ -85,6 +90,28 @@ public class Person implements Serializable {
 
 	public enum Gender {
 		MALE, FEMALE
+	}
+
+	private class PersonProxy implements Serializable {
+		private String data;
+
+		private PersonProxy(Person orig) {
+			data = orig.getFirstName() + "," + orig.getLastName() + "," + orig.getAge() + "," + orig.getGender();
+			if (orig.getSpouse() != null) {
+				Person spouse = orig.getSpouse();
+				data = data + "," + spouse.getFirstName() + "," + spouse.getLastName() + "," + spouse.getAge() + "," + spouse.getGender();
+			}
+		}
+
+		private Object readResolve() throws ObjectStreamException {
+			String[] pieces = data.split(",");
+			Person result = new Person(pieces[0], pieces[1], Integer.parseInt(pieces[2]), Gender.valueOf(pieces[3]));
+			if (pieces.length > 4) {
+				result.setSpouse(new Person(pieces[4], pieces[5], Integer.parseInt(pieces[6]), Gender.valueOf(pieces[7])));
+				result.getSpouse().setSpouse(result);
+			}
+			return result;
+		}
 	}
 
 }
