918cdb0c6aeaa84c8f3d503c8d963e30258e4958
Artur Lomsadze <lomsadze@bpcbt.com>
2017-04-19 08:35:53 +0300

    Added JMX example from Oracle
    link: http://docs.oracle.com/javase/tutorial/jmx/mbeans/standard.html

---
+++ src/main/java/com/ail/home/jmx/oracle/example/HelloMBean.java
@@ -0,0 +1,29 @@
+/*
+ * HelloMBean.java - MBean interface describing the management operations and
+ * attributes for the Hello World MBean. In this case there are two operations,
+ * "sayHello" and "add", and two attributes, "Name" and "CacheSize".
+ */
+
+package com.ail.home.jmx.oracle.example;
+
+public interface HelloMBean {
+	//-----------
+	// operations
+	//-----------
+
+	void sayHello();
+
+	int add(int x, int y);
+
+	//-----------
+	// attributes
+	//-----------
+
+	// a read-only attribute called Name of type String
+	String getName();
+
+	// a read-write attribute called CacheSize of type int
+	int getCacheSize();
+
+	void setCacheSize(int size);
+}
